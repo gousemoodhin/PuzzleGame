89212b9f9ed66118eb009e9af6f23d7ed4c4bac5
Gousemoodhin Nadaf <30875627+gousemoodhin@users.noreply.github.com>
2024-12-25 13:48:34 -0800

    Update game_puzzle.cpp

--- game_puzzle.cpp
+++ game_puzzle.cpp
@@ -2,53 +2,78 @@
 #include <vector>
 using namespace std;
 
-void DisplayElements(vector<vector<int>> vec, int *zero_i, int *zero_j) {
-    for (int i = 0; i < vec.size(); i++) {
-    for (int j = 0; j < vec.size(); j++) {
-      cout << "\t" << vec[i][j] << "\t";
-      if (vec[i][j] == 0) {
-        *zero_i = i;  // Update position of Zero.
-        *zero_j = j;
-      }
+class PuzzleGame {
+private:
+    vector<vector<int>> grid;
+    int zero_row;
+    int zero_col;
+
+public:
+    // Constructor to initialize the puzzle grid.
+    PuzzleGame() : grid({{1, 2, 3}, {4, 5, 6}, {7, 8, 0}}), zero_row(2), zero_col(2) {}
+
+    // Function to display the current state of the puzzle grid.
+    void displayGrid() {
+        for (int row = 0; row < grid.size(); row++) {
+            for (int col = 0; col < grid[row].size(); col++) {
+                cout << "\t" << grid[row][col] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  cout << endl;
-}
+
+    // Function to update the grid based on user input.
+    bool processInput(int user_input) {
+        for (int row = 0; row < grid.size(); row++) {
+            for (int col = 0; col < grid[row].size(); col++) {
+                if (user_input == grid[row][col]) {
+                    if ((col + 1 < grid[row].size() && grid[row][col + 1] == 0) ||
+                        (col - 1 >= 0 && grid[row][col - 1] == 0) ||
+                        (row + 1 < grid.size() && grid[row + 1][col] == 0) ||
+                        (row - 1 >= 0 && grid[row - 1][col] == 0)) {
+
+                        // Swap the input number with zero.
+                        grid[zero_row][zero_col] = user_input;
+                        grid[row][col] = 0;
+
+                        // Update the position of zero.
+                        zero_row = row;
+                        zero_col = col;
+                        return true;
+                    } else {
+                        // Input number is not adjacent to zero.
+                        cout << "\n\n\tInvalid Number!!!\n\n";
+                        return false;
+                    }
+                }
+            }
+        }
+        // Input number not found in the grid.
+        cout << "\n\n\tInvalid Input!!!\n\n";
+        return false;
+    }
+
+    // Function to start the game loop.
+    void playGame() {
+        cout << "\n\n\t\t   ***** PUZZLE GAME *****\n";
+        cout << "\tNote: Enter input number which is adjacent to Zero (0)\n\n\n";
+
+        displayGrid();
+
+        while (true) {
+            int user_input;
+            cout << "\tEnter input number to replace with empty space (Zero): ";
+            cin >> user_input;
+            processInput(user_input);
+            displayGrid();
+        }
+    }
+};
 
 int main() {
-  cout << "\n\n\t\t   ***** PUZZLE GAME ****\n";
-  cout << "\tNote: Enter input number which is adjacent to Zero(0)\n\n\n";
-
-  int input, zero_i, zero_j;
-  vector<vector<int>> vec = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
-  DisplayElements(vec, &zero_i, &zero_j);
-
-  while (true) {
-    cout << "\tEnter input number to replace with empty space(Zero): ";
-    cin >> input;
-    int flag = 0;
-    for (int i = 0; i < vec.size(); i++) {
-      for (int j = 0; j < vec.size(); j++) {
-        if (input == vec[i][j]) {
-          if (vec[i][j + 1] == 0 || vec[i][j - 1] == 0 || vec[i + 1][j] == 0 ||
-              vec[i - 1][j] == 0) {
-            vec[zero_i][zero_j] = input;
-            vec[i][j] = 0;
-            zero_i = i;
-            zero_j = j;
-            flag = 1;
-            break;
-          } else {
-            flag = 1;
-            cout << "\n\n\tInvalid Number!!!\n\n";
-            continue;
-          }
-        }
-      }
-      if (flag == 1)
-        break;
-    }
-    DisplayElements(vec, &zero_i, &zero_j);
-  }
+    // Create an instance of the PuzzleGame class and start the game.
+    PuzzleGame puzzle;
+    puzzle.playGame();
+    return 0;
 }
